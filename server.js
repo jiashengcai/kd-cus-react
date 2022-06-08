faec80c782854772ddab11595be3b4e9c578e097
cacacai <jiashengcai97@email.com>
2022-06-08 09:38:18 +0000

    调整开发商标识，可以正常与本地环境进行测试开发

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const serveIndex = require('serve-index');
 
 //设置跨域访问
 app.all('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://feature.kingdee.com:2024');
+    res.header('Access-Control-Allow-Origin', 'http://172.20.182.97:80');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
     res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
@@ -17,5 +17,5 @@ app.use('/', express.static(path.join(__dirname, './src/web')));
 app.use('/', serveIndex(path.join(__dirname, './src/web'), {'icons': true}));
 
 app.listen(3001, () => { // 默认为localhost（本地主机）; 端口号3001
-    console.log("CustomControl Static Server is Running!")
+    console.log("CustomControl Static Server is Running!","127.0.0.1:3001")
 })
